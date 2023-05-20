50a9414f92881c503d0f8c9991be9a43f9080e1d
kamilkubiak2210 <kamil.kubiak4@outlook.com>
2023-05-20 09:04:32 +0200

    Rozwiązanie.

--- src/main/java/pl/javastart/voting/Vote.java
+++ src/main/java/pl/javastart/voting/Vote.java
@@ -1,5 +1,7 @@
 package pl.javastart.voting;
 
+import java.util.Objects;
+
 // klasa pomocnicza, możesz ją dowolnie zmieniać, albo usunąć
 public class Vote {
 
@@ -11,11 +13,48 @@ public class Vote {
         this.vote = vote;
     }
 
+    public Vote() {
+    }
+
     public String getVoter() {
         return voter;
     }
 
     public Boolean getVote() {
         return vote;
+    }
+
+    @Override
+    public String toString() {
+        return voter + ": " + vote;
+    }
+
+    public String voteToString(Boolean vote) {
+        String voteResult = "";
+        if (vote == null) {
+            voteResult = "WSTRZYMAŁ SIĘ";
+        } else if (vote) {
+            voteResult = "ZA";
+        } else if (!vote) {
+            voteResult = "PRZECIW";
+        }
+        return voteResult;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Vote vote1 = (Vote) o;
+        return Objects.equals(voter, vote1.voter) && Objects.equals(vote, vote1.vote);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(voter, vote);
     }
 }
--- src/main/java/pl/javastart/voting/Voting.java
+++ src/main/java/pl/javastart/voting/Voting.java
@@ -1,6 +1,7 @@
 package pl.javastart.voting;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,14 +12,14 @@ public class Voting {
 
         // możesz dowolnie dodawać / usuwać dane testowe
         voters.add("Jan Kowalski");
-        voters.add("Zigniew Siobro");
+        voters.add("Zbigniew Siobro");
         voters.add("Zbyszek Stonoga");
 
         Voting voting = new Voting();
 
         VotingResult votingResult = voting.executeVoting(voters, new Scanner(System.in));
         votingResult.printResults();
-        votingResult.printVoteForVoter("Zigniew Siobro");
+        votingResult.printVoteForVoter("Zbigniew Siobro");
     }
 
     /**
@@ -27,8 +28,30 @@ public class Voting {
      * Metoda powinna pobrać głos dla każdego przekazanego głosującego i zapisać wyniki głosowania do VotingResult
      */
     VotingResult executeVoting(List<String> voters, Scanner scanner) {
+        List<Vote> voteList = new LinkedList<>();
 
-        return null; // to możesz (a nawet powinieneś/powinnaś) zmienić :)
+        Boolean result = null;
+        for (int i = 0; i < voters.size(); i++) {
+            System.out.print("Jak głosuje " + voters.get(i) + "? ");
+            String line;
+            do {
+                System.out.println("Podaj (z - za, p - przeciw, w - wstrzymanie się)");
+                line = scanner.nextLine();
+                if (line.equals("z") || line.equals("p") || line.equals("w")) {
+                    switch (line) {
+                        case "z" -> result = true;
+                        case "p" -> result = false;
+                        case "w" -> result = null;
+                    }
+                    break;
+                }
+            } while (!line.equals("z") || !line.equals("p") || !line.equals("w"));
+            voteList.add(new Vote(voters.get(i), result));
+        }
+        return new VotingResult(voteList); // to możesz (a nawet powinieneś / powinnaś) zmienić :)
     }
-
 }
+
+
+
+
